a81f33c2c00dd14fc170ef90bfd60fec5732e3fc
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2023-08-16 18:39:01 -0400

    'hw_1done'

---
+++ lesson 1/hw1_js.js
@@ -0,0 +1,50 @@
+//змінні
+a1 = 'hello'
+a2 = 'owu'
+a3 = 'com'
+a4 = 'ua'
+q1 = 1
+q2 = 10
+q3 = -999
+q4 = 123
+p = 3.14
+w1 = 2.7
+w2 = 16
+e1 = `true`
+e2 = `false`
+
+
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a4)
+console.log(q1)
+console.log(q2)
+console.log(q3)
+console.log(q4)
+console.log(p)
+console.log(w1)
+console.log(w2)
+console.log(e1)
+console.log(e2)
+
+//#2
+firstname = 'Volodymyr';
+lastname = 'Vasiutyn';
+middlename = 'Volodymyrovch';
+let person = (firstname + ' ' + lastname + ' ' + middlename)
+console.log(person)
+
+//#3
+let a = 100;
+let b ='100';
+let s = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof s)
+
+//#4
+let name = prompt('ater your name');
+let last = prompt('ater your last name');
+let age = prompt('ater your age');
+console.log(name + ' ' + last + ' ' + age);
