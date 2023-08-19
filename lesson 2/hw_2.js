584bb48d64cd6069eb879f54491a23a3c6ba21b0
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2023-08-18 20:51:02 -0400

    asdasd

---
+++ lesson 2/hw_2.js
@@ -0,0 +1,88 @@
+//- Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let  first = [
+    "asdlasd1",
+    "asdlasd2",
+    "asdlasd3",
+    "asdlasd4",
+    "asdlasd5",
+    "asdlasd6",
+    "asdlasd7",
+    "asdlasd8",
+    "asdlasd9",
+    "asdlasd10"
+]
+console.log(first);
+
+//- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre
+let book = {
+    title: 'asf',
+    pageCount: 'fasf',
+    genre: 'dsfsd'
+};
+let book2 = {
+    title: 'asf',
+    pageCount: 'fasf',
+    genre: 'dsfsd'
+};
+let book3 = {
+    title: 'asf',
+    pageCount: 'fasf',
+    genre: 'dsfsd'
+};
+console.log(book, book2, book3)
+
+//- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.
+let book4 = {
+    title: 'asdsad',
+    pageCount: 'asfaxxas',
+    genre: 'bcvbc',
+    authors: ['name', 'age']
+}
+;let book5 = {
+    title: 'asdsad',
+    pageCount: 'asfaxxas',
+    genre: 'bcvbc',
+    authors: ['name', 'age']
+}
+;let book6 = {
+    title: 'asdsad',
+    pageCount: 'asfaxxas',
+    genre: 'bcvbc',
+    authors: ['name', 'age']
+};
+console.log(book4,book5,book6)
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+let ten = [
+    {name:'asd',username:'dsvds',password:'vffbf'},
+    {name:'asd',username:'dsvds',password:'vffbf'},
+    {name:'asd',username:'dsvds',password:'vffbf'},
+    {name:'asd',username:'dsvds',password:'vffbf'},
+    {name:'asd',username:'dsvds',password:'vffbf'},
+    {name:'asd',username:'dsvds',password:'vffbf'},
+    {name:'asd',username:'dsvds',password:'vffbf'},
+    {name:'asd',username:'dsvds',password:'vffbf'},
+    {name:'asd',username:'dsvds',password:'vffbf'},
+    {name:'asd',username:'dsvds',password:'vffbf'}
+]
+//
+//
+// Логічні розгалуження:
+// - Є змінна х, якій ви надаєте довільне числове значення.
+// Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+// - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+// (в першу, другу, третю или четверту частину години).
+// - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+// - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+// - Користувач вводить або має два числа.
+// Потрібно знайти та вивести максимальне число з тих двох .
+// Також потрібно врахувати коли введені рівні числа.
+//
+// - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+// за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
+//
+//
+// - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+
+
+
