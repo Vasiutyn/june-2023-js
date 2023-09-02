ffeb965f9b54cfba290fb5c5deca88efd084a1e7
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2023-09-02 18:28:17 -0400

    asdasd

--- lesson 5/hw_5.js
+++ lesson 5/hw_5.js
@@ -123,13 +123,10 @@ console.log(unswer2);
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 const swap = (arr,index1,index2) => {
-    let a = arr[index1];
-    let b = arr[index2];
-    arr[index1] = b
-    arr[index2] = a
-    console.log(arr)
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+    return arr
 }
-swap([11,22,33,44],0,1)
+console.log(swap([11,22,33,44],0,1))
 
 // ----------------------------------------------------------------------
 
